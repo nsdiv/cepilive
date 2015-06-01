11298c7fc9ae9aee3e975738bd6364072ec7ab27
Ninad Divadkar <ninad_divadkar@intuit.com>
2015-06-01 10:07:35 -0700

    cepi data

---
+++ cepi-data/src/main/java/com/cepi/data/repo/IndividualTService.java
@@ -0,0 +1,62 @@
+package com.cepi.data.repo;
+
+import com.cepi.data.IndividualTEntity;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.metadata.ClassMetadata;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import java.util.Map;
+
+/**
+ * Created by ndivadkar on 5/31/15.
+ */
+public class IndividualTService {
+    private static final SessionFactory ourSessionFactory;
+    private static final ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            ourSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public IndividualTEntity getIndividualT(String email){
+        final Session session = getSession();
+        try {
+            final Query query = session.createQuery("from IndividualTEntity where emailAddress=" + email);
+            for (Object o : query.list()) {
+                System.out.println("  " + o);
+                return (IndividualTEntity)o;
+            }
+//            System.out.println("querying all the managed entities...");
+//            final Map metadataMap = session.getSessionFactory().getAllClassMetadata();
+//            for (Object key : metadataMap.keySet()) {
+//                final ClassMetadata classMetadata = (ClassMetadata) metadataMap.get(key);
+//                final String entityName = classMetadata.getEntityName();
+//                final Query query = session.createQuery("from " + entityName);
+//                System.out.println("executing: " + query.getQueryString());
+//                for (Object o : query.list()) {
+//                    System.out.println("  " + o);
+//                }
+//            }
+        } finally {
+            session.close();
+        }
+        return null;
+    }
+}
--- cepi/src/main/java/com/cepi/mvc/HelloController.java
+++ cepi/src/main/java/com/cepi/mvc/HelloController.java
@@ -1,5 +1,7 @@
 package com.cepi.mvc;
 
+import com.cepi.data.IndividualTEntity;
+import com.cepi.data.repo.IndividualTService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,9 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
+
+        IndividualTService service = new IndividualTService();
+        IndividualTEntity entity = service.getIndividualT("susan@baurmann.net");
 		return "hello";
 	}
 }
